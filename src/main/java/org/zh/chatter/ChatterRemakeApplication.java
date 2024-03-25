426aff8249d824c162047e761d32527405e242b8
zenox <13506553843@163.com>
2024-03-25 16:34:10 +0800

    第一次提交

---
+++ src/main/java/org/zh/chatter/ChatterRemakeApplication.java
@@ -0,0 +1,13 @@
+package org.zh.chatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatterRemakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatterRemakeApplication.class, args);
+    }
+
+}
