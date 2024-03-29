2b8e2e5e3e9ae215edd48eaeb631def6ba5a6505
zenox <13506553843@163.com>
2024-03-29 09:22:39 +0800

    引入javafx

--- src/main/java/org/zh/chatter/ChatterRemakeApplication.java
+++ src/main/java/org/zh/chatter/ChatterRemakeApplication.java
@@ -1,13 +1,31 @@
 package org.zh.chatter;
 
-import org.springframework.boot.SpringApplication;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URL;
+
 @SpringBootApplication
-public class ChatterRemakeApplication {
+public class ChatterRemakeApplication extends Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatterRemakeApplication.class, args);
+        launch(args);
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        URL resource = getClass().getResource("scene.fxml");
+        Parent root = FXMLLoader.load(resource);
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+        stage.setTitle("JavaFX and Gradle");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
