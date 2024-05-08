8d9de0117a91f4a3ccab7535ca2ee917abd96afa
zenox <13506553843@163.com>
2024-05-08 14:33:16 +0800

    补充UI单元测试；修改用户名改为异步操作

--- src/main/java/org/zh/chatter/controller/TopMenuBarController.java
+++ src/main/java/org/zh/chatter/controller/TopMenuBarController.java
@@ -5,16 +5,17 @@ import jakarta.annotation.Resource;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextInputDialog;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.zh.chatter.manager.CurrentUserInfoHolder;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 
 @Controller
 public class TopMenuBarController {
@@ -22,6 +23,8 @@ public class TopMenuBarController {
     private static final String ABOUT_FILE_PATH = "about.txt";
     private static final String MODIFY_USERNAME_DIALOG_TITLE = "修改用户名";
     private static final String MODIFY_USERNAME_DIALOG_HEADER_TEXT = "请输入用户名";
+    private static final String INPUT_INVALID_TITLE = "输入有误";
+    private static final String INPUT_INVALID_CONTENT_TEXT = "输入不能为空";
     @FXML
     private MenuBar topMenuBar;
 
@@ -46,7 +49,23 @@ public class TopMenuBarController {
         TextInputDialog textInputDialog = new TextInputDialog(currentUserInfoHolder.getCurrentUser().getUsername());
         textInputDialog.setTitle(MODIFY_USERNAME_DIALOG_TITLE);
         textInputDialog.setHeaderText(MODIFY_USERNAME_DIALOG_HEADER_TEXT);
-        Optional<String> newUsername = textInputDialog.showAndWait();
-        newUsername.ifPresent(u -> currentUserInfoHolder.getCurrentUser().setUsername(u));
+        textInputDialog.show();
+        textInputDialog.setResultConverter(buttonType -> {
+            String username = "";
+            if (buttonType == ButtonType.OK) {
+                String result = textInputDialog.getEditor().getText();
+                if (Strings.isNotEmpty(result)) {
+                    currentUserInfoHolder.getCurrentUser().setUsername(result);
+                    username = result;
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle(INPUT_INVALID_TITLE);
+                    alert.setHeaderText(null);
+                    alert.setContentText(INPUT_INVALID_CONTENT_TEXT);
+                    alert.show();
+                }
+            }
+            return username;
+        });
     }
 }
---
+++ src/test/java/org/zh/chatter/UserInterfaceTests.java
@@ -0,0 +1,114 @@
+package org.zh.chatter;
+
+import cn.hutool.core.util.RandomUtil;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableView;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.testfx.api.FxAssert;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+import org.testfx.matcher.control.ListViewMatchers;
+import org.testfx.matcher.control.TableViewMatchers;
+import org.testfx.util.WaitForAsyncUtils;
+import org.zh.chatter.manager.CurrentUserInfoHolder;
+import org.zh.chatter.model.bo.NodeUserBO;
+import org.zh.chatter.model.vo.ChatMessageVO;
+import org.zh.chatter.model.vo.UserVO;
+
+import java.util.concurrent.TimeUnit;
+
+@ExtendWith(ApplicationExtension.class)
+@SpringBootTest
+public class UserInterfaceTests {
+
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+    private static final String FXML_PATH = "fxml/main.fxml";
+    private static final String CSS_PATH = "css/styles.css";
+    private static final String WINDOW_TITLE = "局域网聊天室";
+
+    private Stage stage;
+    @Autowired
+    private CurrentUserInfoHolder currentUserInfoHolder;
+
+    @Start
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(new ClassPathResource(FXML_PATH).getURL());
+        loader.setControllerFactory(applicationContext::getBean);
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(new ClassPathResource(CSS_PATH).getURL().toExternalForm());
+        stage.setTitle(WINDOW_TITLE);
+        stage.setScene(scene);
+        stage.show();
+        this.stage = stage;
+    }
+
+    @Test
+    public void shouldExitApp(FxRobot robot) throws InterruptedException {
+        MenuBar bar = robot.lookup("#topMenuBar").query();
+        MenuItem menuItem = bar.getMenus().get(0).getItems().get(1);
+        robot.interactNoWait(menuItem::fire);
+        Assertions.assertFalse(stage.isShowing());
+    }
+
+    @Test
+    public void testModifyUsername(FxRobot robot) throws Exception {
+        MenuBar bar = robot.lookup("#topMenuBar").query();
+        MenuItem menuItem = bar.getMenus().get(0).getItems().get(0);
+        robot.interactNoWait(menuItem::fire);
+        //等两秒钟直到弹窗显示
+        WaitForAsyncUtils.waitFor(2, TimeUnit.SECONDS, () -> robot.lookup(".dialog-pane").query().isVisible());
+        String newUsername = RandomUtil.randomString(5);
+        robot.write(newUsername);
+        robot.press(KeyCode.ENTER);
+        Assertions.assertEquals(newUsername, currentUserInfoHolder.getCurrentUser().getUsername());
+    }
+
+    @Test
+    public void testSendMessage(FxRobot robot) {
+        String inputContent = RandomUtil.randomString(100);
+        robot.clickOn("#inputArea").write(inputContent);
+        robot.clickOn("#sendButton");
+        FxAssert.verifyThat("#messageArea", ListViewMatchers.hasItems(1));
+        ListView listView = robot.lookup("#messageArea").queryAs(ListView.class);
+        NodeUserBO currentUser = currentUserInfoHolder.getCurrentUser();
+        boolean messagePresent = listView.getItems().stream().anyMatch(l -> {
+            ChatMessageVO message = (ChatMessageVO) l;
+            return message.getMessage().equals(inputContent) && message.getSenderId().equals(currentUser.getId()) && message.getSenderName().equals(currentUser.getUsername());
+        });
+        Assertions.assertTrue(messagePresent);
+    }
+
+    @Test
+    public void testClearChatArea(FxRobot robot) {
+        this.testSendMessage(robot);
+        robot.clickOn("#clearButton");
+        FxAssert.verifyThat("#messageArea", ListViewMatchers.hasItems(0));
+    }
+
+    @Test
+    public void testUserListDialog(FxRobot robot) {
+        robot.clickOn("#userListButton");
+        FxAssert.verifyThat(".table-view", TableViewMatchers.hasNumRows(1));
+        NodeUserBO currentUser = currentUserInfoHolder.getCurrentUser();
+        TableView tableView = robot.lookup(".table-view").queryAs(TableView.class);
+        boolean hasUser = tableView.getItems().contains(new UserVO(currentUser.getId(), currentUser.getUsername(), true));
+        Assertions.assertTrue(hasUser);
+    }
+}
